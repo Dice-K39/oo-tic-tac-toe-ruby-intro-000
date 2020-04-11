970c26fb5c8034a2bf70cc2a2d081c2472bf2d4c
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2020-04-11 18:37:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,9 +41,16 @@ class TicTacToe
     end
   end
   
+  def turn_count
+    counter = 0
   
+    @board.each do |board_space|
+      if board_space == "X" || board_space == "O"
+        counter += 1
+      end
+    end
+  
+    counter
+  end
 end
-
-
-
 
